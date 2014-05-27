51e353e8f71b74acb328c1e242ce1ab01304c1dc
haojg <443842006@qq.com>
2014-05-27 09:02:55 +0800

    测试netty框架，一个http请求例子，一个是后台socket通信的例子

---
+++ testnetty/src/main/java/hjg/http/AdminServer.java
@@ -0,0 +1,48 @@
+package hjg.http;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
+import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+
+/**
+ * 后台管理服务
+ */
+public class AdminServer {
+	public static void main(String[] args) {
+		start(8080);
+	}
+
+	public static void start(int port) {
+		// 配置服务器-使用java线程池作为解释线程
+		ServerBootstrap bootstrap = new ServerBootstrap(
+				new NioServerSocketChannelFactory(
+						Executors.newCachedThreadPool(),//boss线程池
+						  Executors.newCachedThreadPool()//worker线程池
+						));
+		// 设置 pipeline factory.
+		bootstrap.setPipelineFactory(new ServerPipelineFactory());
+		// 绑定端口
+		bootstrap.bind(new InetSocketAddress(port));
+		System.out.println("server start on " + port);
+	}
+
+	private static class ServerPipelineFactory implements
+			ChannelPipelineFactory {
+		public ChannelPipeline getPipeline() throws Exception {
+			// Create a default pipeline implementation.
+			ChannelPipeline pipeline = Channels.pipeline();
+			pipeline.addLast("decoder", new HttpRequestDecoder());
+			pipeline.addLast("encoder", new HttpResponseEncoder());
+			// http处理handler
+			pipeline.addLast("handler", new AdminServerHandler());
+			return pipeline;
+		}
+	}
+}
