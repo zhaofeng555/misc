f484d294da09e7f6c1f82ec1b21120dfc61c21dc
haojg <443842006@qq.com>
2014-06-12 10:48:19 +0800

    Enum例子

---
+++ testmisc/src/main/java/hjg/collection/TestEnumMethod.java
@@ -0,0 +1,11 @@
+package hjg.collection;
+
+public class TestEnumMethod {
+	 
+	public static void main(String[] args) {
+ 
+		System.out.println(UserStatus.ACTIVE.getStatusCode());
+ 
+	}
+ 
+}
---
+++ testmisc/src/main/java/hjg/collection/UserStatus.java
@@ -0,0 +1,16 @@
+package hjg.collection;
+
+public enum UserStatus {
+	PENDING("P"), ACTIVE("A"), INACTIVE("I"), DELETED("D");
+ 
+	private String statusCode;
+ 
+	private UserStatus(String s) {
+		statusCode = s;
+	}
+ 
+	public String getStatusCode() {
+		return statusCode;
+	}
+ 
+}
