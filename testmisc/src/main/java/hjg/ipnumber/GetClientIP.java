d32aeb266b004bbd483ac20cb529c4ddcc9fcfe4
haojg <443842006@qq.com>
2014-06-12 15:12:50 +0800

    服务器端获取客户端的ip地址

---
+++ testmisc/src/main/java/hjg/ipnumber/GetClientIP.java
@@ -0,0 +1,15 @@
+package hjg.ipnumber;
+
+public class GetClientIP {
+
+	public static void main(String[] args) {
+		//is client behind something?
+		
+//		   String ipAddress = request.getHeader("X-FORWARDED-FOR");  
+//		   if (ipAddress == null) {  
+//			   ipAddress = request.getRemoteAddr();  
+//		   }
+		
+	}
+	
+}
