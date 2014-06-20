24ed690f00685cac97101a23ff823632d23375c5
haojg <443842006@qq.com>
2014-06-20 09:08:28 +0800

    获取客户端ip地址

--- testmisc/src/main/java/hjg/ipnumber/GetClientIP.java
+++ testmisc/src/main/java/hjg/ipnumber/GetClientIP.java
@@ -1,5 +1,11 @@
 package hjg.ipnumber;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class GetClientIP {
 
 	public static void main(String[] args) {
@@ -11,5 +17,29 @@ public class GetClientIP {
 //		   }
 		
 	}
-	
+
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String ip = getClientIpAddr(request);
+		System.out.println(ip);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	public String getClientIpAddr(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
+	}
+
 }
