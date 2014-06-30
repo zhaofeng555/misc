f281f6dcd17206fa9e20f9871addba5c3669fdd1
haojg <443842006@qq.com>
2014-06-30 09:48:18 +0800

    输出数组工具类

---
+++ testmisc/src/main/java/hjg/mianshi/Utils.java
@@ -0,0 +1,13 @@
+package hjg.mianshi;
+
+public class Utils {
+
+	public static void printArray(String string, int[] arr) {
+		System.out.println(string);
+		for (int a : arr) {
+			System.out.print(a+"\t");
+		}
+		System.out.println();
+	}
+
+}
