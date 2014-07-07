c31f2c76a0011a11dd6e7a6091f2c360d5332940
haojg <443842006@qq.com>
2014-07-07 11:14:06 +0800

    字符串比较，分拆字符串为单个字母在逐个比较

---
+++ testmisc/src/main/java/hjg/mianshi/SortString.java
@@ -0,0 +1,42 @@
+package hjg.mianshi;
+
+public class SortString {
+
+	public static void main(String[] args) {
+		String s1 = "hell0";
+		String s2 = "hel0l";
+		String s3 = "ss";
+		System.out.println(anagram(s1, s2));
+		System.out.println(anagram(s1, s3));
+	}
+
+	public static boolean anagram(String s, String t) {
+		if (s.length() != t.length())
+			return false;
+		int[] letters = new int[256];
+		int num_unique_chars = 0;
+		int num_completed_t = 0;
+		char[] s_array = s.toCharArray();
+		for (char c : s_array) { // count number of each char in s.
+			if (letters[c] == 0)
+				++num_unique_chars;
+			++letters[c];
+		}
+		
+		for (int i = 0; i < t.length(); ++i) {
+			int c = (int) t.charAt(i);
+			if (letters[c] == 0) { // Found more of char c in t than in s.
+				return false;
+			}
+			--letters[c];
+			if (letters[c] == 0) {
+				++num_completed_t;
+				if (num_completed_t == num_unique_chars) {
+					// it’s a match if t has been processed completely
+					return i == t.length() - 1;
+				}
+			}
+		}
+		return false;
+	}
+}
