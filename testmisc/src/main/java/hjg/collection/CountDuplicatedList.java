e103a0b201817614410cc997ae068de36154be1a
Administrator <Administrator@DP3SL8RRRS6AHJP>
2016-08-17 11:27:17 +0800

    集合操作

--- testmisc/src/main/java/hjg/collection/CountDuplicatedList.java
+++ testmisc/src/main/java/hjg/collection/CountDuplicatedList.java
@@ -11,41 +11,57 @@ import java.util.TreeMap;
  
 public class CountDuplicatedList {
  
+	public static void test1(){
+		List<String> ids = new ArrayList<String>();
+		ids.add("1");
+		ids.add("6");
+		ids.add("5");
+		ids.add("4");
+		ids.add("3");
+		
+		Collections.sort(ids);
+		System.out.println(ids);
+	}
+	
   public static void main(String[] args) {
+ test1();
  
-	List<String> list = new ArrayList<String>();
-	list.add("a");
-	list.add("b");
-	list.add("c");
-	list.add("d");
-	list.add("b");
-	list.add("c");
-	list.add("a");
-	list.add("a");
-	list.add("a");
- 
-	System.out.println("\nExample 1 - Count 'a' with frequency");
-	System.out.println("a : " + Collections.frequency(list, "a"));
- 
-	System.out.println("\nExample 2 - Count all with frequency");
-	Set<String> uniqueSet = new HashSet<String>(list);
-	for (String temp : uniqueSet) {
-		System.out.println(temp + ": " + Collections.frequency(list, temp));
-	}
- 
-	System.out.println("\nExample 3 - Count all with Map");
-	Map<String, Integer> map = new HashMap<String, Integer>();
- 
-	for (String temp : list) {
-		Integer count = map.get(temp);
-		map.put(temp, (count == null) ? 1 : count + 1);
-	}
-	printMap(map);
- 
-	System.out.println("\nSorted Map");
-	Map<String, Integer> treeMap = new TreeMap<String, Integer>(map);
-	printMap(treeMap);
- 
+  }
+  
+  public static void test2(){
+
+		List<String> list = new ArrayList<String>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("d");
+		list.add("b");
+		list.add("c");
+		list.add("a");
+		list.add("a");
+		list.add("a");
+	 
+		System.out.println("\nExample 1 - Count 'a' with frequency");
+		System.out.println("a : " + Collections.frequency(list, "a"));
+	 
+		System.out.println("\nExample 2 - Count all with frequency");
+		Set<String> uniqueSet = new HashSet<String>(list);
+		for (String temp : uniqueSet) {
+			System.out.println(temp + ": " + Collections.frequency(list, temp));
+		}
+	 
+		System.out.println("\nExample 3 - Count all with Map");
+		Map<String, Integer> map = new HashMap<String, Integer>();
+	 
+		for (String temp : list) {
+			Integer count = map.get(temp);
+			map.put(temp, (count == null) ? 1 : count + 1);
+		}
+		printMap(map);
+	 
+		System.out.println("\nSorted Map");
+		Map<String, Integer> treeMap = new TreeMap<String, Integer>(map);
+		printMap(treeMap);
   }
  
   public static void printMap(Map<String, Integer> map){
