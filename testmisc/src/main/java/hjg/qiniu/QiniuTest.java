667ecb17b69e4bbf9401c9e188eb9adf4347e3df
Administrator <Administrator@DP3SL8RRRS6AHJP>
2016-08-17 11:37:04 +0800

    骑牛

--- testmisc/src/main/java/hjg/qiniu/QiniuTest.java
+++ testmisc/src/main/java/hjg/qiniu/QiniuTest.java
@@ -14,9 +14,6 @@ public class QiniuTest {
 	final static String ak = "EjRSYSXQDm-ihlBoF9vG4vHPK0dSxwLYDnwxMgUv";
 	final static String sk = "7OtL5GSXUtlygYEUmxU2j5fXoWcWpvWz0n1mor6z";
 	final static String bucketName = "hnb-img0";
-//	final static String ak = "gx37fl79RG1QGc0DHddKTTARO2GzbeilG18mui-I";
-//	final static String sk = "5lLL2wNXdJeD2zeSEZLc8GdQUVW7qVfBA8rCFfs5";
-//	String bucketName = "haojg";
 	
 	public static void init() throws AuthException, JSONException {
 		Config.ACCESS_KEY = ak;
@@ -24,13 +21,12 @@ public class QiniuTest {
 		
 		Mac mac = new Mac(Config.ACCESS_KEY, Config.SECRET_KEY);
         // 请确保该bucket已经存在
-//        String bucketName = "haojg";
         PutPolicy putPolicy = new PutPolicy(bucketName);
         String uptoken = putPolicy.token(mac);
         
         PutExtra extra = new PutExtra();
-        String key = "动物-考拉";
-        String localFile = "d:/kaola.jpg";
+        String key = "touxiang_user";
+        String localFile = "d:/ko.jpg";
         PutRet ret = IoApi.putFile(uptoken, key, localFile, extra);
         System.out.println(ret.getStatusCode());
         System.out.println(ret.getResponse());
